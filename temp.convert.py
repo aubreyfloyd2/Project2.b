3133b109f606b274c9984542cf6392f5cf8861f1
Aubrey Floyd <aubreyfloyd2@gmail.com>
2023-01-18 12:06:27 -0600

    convert C to F

---
+++ temp.convert.py
@@ -0,0 +1,9 @@
+# Author: Aubrey Floyd
+# GitHub username: aubreyfloyd2
+# Date: 1/18/2023
+# Description: Converts Celsius temperatures to Fahrenheit temperatures
+print("Please enter Celsius temperature.")
+Celsius = float(input())
+Fahrenheit = float(((9 / 5) * Celsius) + 32)
+print("The equivalent Fahrenheit temperature is:")
+print(Fahrenheit)
